24b6a652ba4ffda3ed35ba9a6f082044c55544b8
Marian Bonhomme <marian.bonhomme@gmail.com>
2023-04-12 16:58:42 +0200

    TP08

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -8,17 +8,20 @@ public class TestBanque {
 	public static void main(String[] args) {
 		Compte bq1 = new Compte(34837, 10000);
 		String chaine = bq1.toString();
-//		System.out.println(chaine);
+		System.out.println(chaine);
 		
 		Compte bq2 = new CompteTaux(58478, 100, 10);
 		String chaine2 = bq2.toString();
-//		System.out.println(chaine2);
+		System.out.println(chaine2);
 		
+		System.out.println("-----------------------------------------");
 		
 		Compte[] tableau = {bq1, bq2};
 		
 		for (Compte i : tableau) {
 			System.out.println(i.toString());
 		}
+		
+		
 	}
 }
---
+++ src/fr/diginamic/banque/entites/TestOperation.java
@@ -0,0 +1,34 @@
+package fr.diginamic.banque.entites;
+
+import fr.diginamic.banque.entites.Operation;
+import fr.diginamic.banque.entites.Credit;
+import fr.diginamic.banque.entites.Debit;
+
+public class TestOperation {
+
+	public static void main(String[] args) {
+		Operation op1 = new Credit("18/03 ", 130.0);
+		Operation op2 = new Debit("09/04 ", 10.3);
+		Operation op3 = new Debit("13/08 ", 1450.0);
+		Operation op4 = new Credit("05/02 ", 26.5);
+		
+		Operation[] operations = {op1, op2, op3, op4};
+		
+		for (Operation i : operations) {
+			System.out.println(i.getType()+i.toString());
+		}
+		
+		double total = 0;
+		
+		for (Operation i : operations) {
+			if(i.getType() == "CREDIT | ") {
+				
+				total = total + i.getMontant();
+			} else {
+				total = total - i.getMontant();
+			}
+		}
+		
+		System.out.println(total);
+	}
+}
